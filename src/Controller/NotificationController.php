a4c16f5e6dd07d01b55df88b4e84f848127a8b5f
darklee36 <dark.lee36@gmail.com>
2020-02-20 10:50:08 +0100

    fix error and add authentification for send notification

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\UserSubscription;
 use Minishlink\WebPush\MessageSentReport;
 use Minishlink\WebPush\WebPush;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -17,58 +19,91 @@ use Symfony\Component\Routing\Annotation\Route;
 class NotificationController extends AbstractController
 {
     /**
-     * @param $request
-     * @param $user
+     * @param Request $request
      * @return JsonResponse
      *
-     * @Route("/register/", name="api_notification_register")
-     * @Method("POST")
+     * @Route("/register/", name="api_notification_register", methods={"POST"})
      */
-    public function registerSubscription($request, $user)
+    public function registerSubscription(Request $request)
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+//        try {
+//            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+//            $user = $this->getUser();
+//        } catch (\Exception $e) {
+//            return JsonResponse::create([
+//                "code" => 401,
+//                "message" => "Authentication required",
+//            ], 401);
+//        }
 
-        $endpoint = $request->request->get("endpoint");
-        $keys = $request->request->get("keys");
+        $user = $this->getDoctrine()->getRepository(User::class)->find(1);
 
-        if ((empty($keys) || !is_array($keys) )|| empty($endpoint)) {
+         $body = json_decode($request->getContent(), true);
+
+        if (!is_array($body["keys"]) || empty($body["endpoint"])) {
             return JsonResponse::create([
                 "code" => 400,
                 "message" => "Missing data to register the subscription (endpoint, keys)",
             ], 400);
         }
 
-        $userSubscription = new UserSubscription();
-        $userSubscription->setUser($this->getUser());
-        $userSubscription-> setEndpoint($endpoint);
-        $userSubscription->setAuthToken($keys["p256dh"]);
-        $userSubscription->setPublicKey($keys["auth"]);
+        try {
+            $endpoint = $body["endpoint"];
+            $keys = $body["keys"];
 
-        $entityManager = $this->getDoctrine()->getManager();
+            $userSubscription = new UserSubscription();
+            $userSubscription->setUser($user);
+            $userSubscription-> setEndpoint($endpoint);
+            $userSubscription->setAuthToken($keys["p256dh"]);
+            $userSubscription->setPublicKey($keys["auth"]);
 
-        $entityManager->persist($userSubscription);
-        $entityManager->flush();
+            $entityManager = $this->getDoctrine()->getManager();
 
-        return JsonResponse::create([
-            "code" => 200,
-        ], 200);
+            $entityManager->persist($userSubscription);
+            $entityManager->flush();
+
+            return JsonResponse::create([
+                "code" => 200,
+            ], 200);
+        } catch (\Exception $e) {
+            return JsonResponse::create([
+                "code" => 500,
+                "" => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
-     * @param $request
+     * @param Request $request
      * @return JsonResponse
      * @throws \ErrorException
      *
-     * @Route("/send/", name="api_notification_register")
+     * @Route("/send/", name="api_notification_send", methods={"POST"})
      */
-    public function sendNotification($request)
+    public function send(Request $request)
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+//        try {
+//            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+//        } catch (\Exception $e) {
+//            return JsonResponse::create([
+//                "code" => 401,
+//                "message" => "Authentication required",
+//            ], 401);
+//        }
 
-        $userIdList = $request->request->get("userIdList");
-        $messageNotification = $request->request->get("messageNotification");
-        $titleNotification = $request->request->get("titleNotification");
-        $linkNotification = $request->request->get("linkNotification");
+        $body = json_decode($request->getContent(), true);
+
+        if ( !array($body["userIdList"]) || empty($body["messageNotification"]) ) {
+            return JsonResponse::create([
+                "code" => 400,
+                "message" => "Missing data to send notification (userIdList, )",
+            ], 400);
+        }
+
+        $userIdList = $body["userIdList"];
+        $messageNotification = $body["messageNotification"];
+        $titleNotification = $body["titleNotification"];
+        $linkNotification = $body["linkNotification"];
 
         $userSubscriptionList = $this->getDoctrine()->getRepository(UserSubscription::class)->findByIds($userIdList);
 
@@ -86,7 +121,14 @@ class NotificationController extends AbstractController
             ];
         }
 
-        $webPush = new WebPush();
+        $webPush = new WebPush([
+            'GCM' => 'MY_GCM_API_KEY', // deprecated and optional, it's here only for compatibility reasons
+            'VAPID' => [
+                'subject' => 'pdelamotte@esimed.fr', // can be a mailto: or your website address
+                'publicKey' => 'BItoVHgEImb1oDvlVaCchsKAHOQNjbclddgfp7mlfZUmKwMIgqlgI5t8bBYRWWNlic3uViiU4ZbIRU6rmRuX_Ac', // (recommended) uncompressed public key P-256 encoded in Base64-URL
+                'privateKey' => '2ro62CCZGMJOJXDX4p1ATSM5MdZLcQrFVQPk-Liu9o4', // (recommended) in fact the secret multiplier of the private key encoded in Base64-URL
+            ],
+        ]);
 
         // send multiple notifications with payload
         foreach ($notifications as $notification) {
@@ -97,6 +139,9 @@ class NotificationController extends AbstractController
             );
         }
 
+//        these line are for debug
+//        $notificationsStatus = [];
+//
 //        /**
 //         * Check sent results
 //         * @var MessageSentReport $report
@@ -105,14 +150,18 @@ class NotificationController extends AbstractController
 //            $endpoint = $report->getRequest()->getUri()->__toString();
 //
 //            if ($report->isSuccess()) {
-//                echo "[v] Message sent successfully for subscription {$endpoint}.";
+//                $notificationsStatus["success"][] = "[v] Message sent successfully for subscription {$endpoint}";
 //            } else {
-//                echo "[x] Message failed to sent for subscription {$endpoint}: {$report->getReason()}";
+//                $notificationsStatus["error"][] = [
+//                    "message" => "[x] Message failed to sent for subscription {$endpoint}",
+//                    "reason" => "{$report->getReason()}"
+//                ];
 //            }
 //        }
 
         return JsonResponse::create([
             "code" => 200,
+//            "data" => $notificationsStatus,
         ], 200);
     }
 }
