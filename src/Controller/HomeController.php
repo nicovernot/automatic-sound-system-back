211cdc149da03721c804fadf49563a5ddd30d427
Kérian <kerianmontes@gmail.com>
2020-02-21 15:11:09 +0100

    clean home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use vendor\google\apiclient;
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
@@ -15,69 +13,31 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-       
-           
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-          
-        ]);
+        return $this->redirect('/api');
     }
 
-      /**
-     * @Route("/playlist", name="playlist")
-     */
-    public function playlist(Request $request)
-    {
-           $client1 = HttpClient::create();
-            $apiKey = "AIzaSyB7CdRuc8guFqf0plkQc826nsEvINljutQ";
-            $playlist_id =  $request->query->get('playlist'); 
-            $api_url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId='.$playlist_id . '&key='.$apiKey;
-                  
-            $playlist = $client1->request('GET',$api_url);
-            $playlist = $playlist->getContent();
-            $playlist =json_decode( $playlist,true);
-            $playlistitems = $playlist["items"];
-            $pl_array=[];
-            $plline=[];
-            foreach($playlistitems as $pl) {
-               $plline["id"]=$pl["snippet"]["resourceId"]["videoId"];
-               $plline["title"]=$pl["snippet"]["title"];
-               $plline["desc"]=$pl["snippet"]["description"];
-               $plline["imgurl"]=$pl["snippet"]["thumbnails"]["high"]["url"];
-               array_push($pl_array, $plline);
-               $plline=[];
-            }
-            return $this->json(['playlist' => $pl_array]);
-      //  return $this->render('home/playlist.html.twig', [
-     //       'controller_name' => 'HomeController',
-     //       "playlist" => $playlist,
-     //   ]);
-    }
-
-
-     /**
+    /**
      * @Route("/singlevideo", name="singlevideo")
      */
     public function singlevideo(Request $request)
     {
-        $search=$request->query->get('search');
+        $search = $request->query->get('search');
         $apiKey = "AIzaSyB7CdRuc8guFqf0plkQc826nsEvINljutQ";
         $client = new \Google_Client();
-        $client -> setDeveloperKey($apiKey);
+        $client->setDeveloperKey($apiKey);
         $youtube = new \Google_Service_YouTube($client);
-        $resp = $youtube->search->listsearch('id,snippet',['q'=>$search,'maxResults'=>10]);
+        $resp = $youtube->search->listsearch('id,snippet', ['q' => $search, 'maxResults' => 10]);
         $playlistitems = $resp["items"];
-        $pl_array=[];
-        $plline=[];
-        foreach($playlistitems as $pl) {
-           $plline["id"]=$pl["snippet"]["resourceId"]["videoId"];
-           $plline["title"]=$pl["snippet"]["title"];
-           $plline["desc"]=$pl["snippet"]["description"];
-           $plline["imgurl"]=$pl["snippet"]["thumbnails"]["high"]["url"];
-           array_push($pl_array, $plline);
-           $plline=[];
+        $pl_array = [];
+        $plline = [];
+        foreach ($playlistitems as $pl) {
+            $plline["id"] = $pl["snippet"]["resourceId"]["videoId"];
+            $plline["title"] = $pl["snippet"]["title"];
+            $plline["desc"] = $pl["snippet"]["description"];
+            $plline["imgurl"] = $pl["snippet"]["thumbnails"]["high"]["url"];
+            array_push($pl_array, $plline);
+            $plline = [];
         }
         return $this->json(['videolist' => $pl_array]);
-      
     }
 }
